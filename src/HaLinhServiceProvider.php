6b8cad5a19e5f9ed6fdb8b29d71a7362c3933900
phudev <codethue94@gmail.com>
2024-05-10 01:39:36 +0700

    init project

---
+++ src/HaLinhServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: trungphuna
+ * Date: 5/10/24
+ * Time: 1:36 AM
+ */
+
+namespace Halinh\Helpers;
+use \Illuminate\Support\ServiceProvider;
+
+class HaLinhServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
